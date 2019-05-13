a29d69f6de4a6b8115a5eb417863c095c8c878a8
Charles Jackson <charles@localhost.localdomain>
2019-05-13 12:01:59 -0500

    *** empty log message ***

--- main.pl
+++ main.pl
@@ -13,16 +13,23 @@ my $user = <STDIN>;		     # get username from console
 print("password: ");		     # ask user for mySQL password
 my $pass = <STDIN>;		     # get password from console
 my $db = DBI->connect("","$user","$pass"); # log in to mySQL server
-$db->do("create table [if not exists] plants (
-	  order VARCHAR(100),
-	  family VARCHAR(100),
-	  genus VARCHAR(100),
-	  species VARCHAR(100));"); # create a table for the orders
-
+## create tables ##
+my $db->do("create table [if not exists] orders 
+	  (order text, order_id integer auto_increment);");
+my $db->do("create table [if not exists] family
+	  (order_id integer, family text, family_id integer auto_increment);");
+my $db->do("create table [if not exists] genus
+	  (family_id integer, genus text, genus_id integer auto_increment);");
+my $db->do("create table [if not exists] species
+	  (genus_id integer, species text, species_id integer auto_increment);");
+my $db->do("create table [if not exists] varieties
+	  (species_id integer, name text);");
+my $db->do("create table [if not exists] synonyms
+	  (species_id integer, nym text);");
 ### initialize psudo-browser ###
-our %depths = (order => 0, # enumerated type to to keep trac of the depth of taxonomy
-	       family => 1,
-	       genus => 2,
+our %depths = (order   => 0, # enumerated type to to keep trac of the depth of taxonomy
+	       family  => 1,
+	       genus   => 2,
 	       species => 3);
 my $bro = WWW::Mechanize->new();	# instantiate browser
 $bro->get("http://www.worldfloraonline.org/classification"); # request website
